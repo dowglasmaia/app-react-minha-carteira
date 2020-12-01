7971836726ebb4b9fffd6b15bebbc02eede7fb3e
Dowglas Maia <dowglasmaia@gmail.com>
2020-12-01 15:30:06 -0300

    estilizando butões de tipo de finança

--- src/Pages/List/index.tsx
+++ src/Pages/List/index.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 import ContentHeader from '../../components/shared/ContentHeader';
 
 import SelectInput from '../../components/shared/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
-    const options = [
-        { value: 'Dowglas Maia', label: 'Dowglas Maia' },
-        { value: 'kayron Maia', label: 'Kayron Maia' },
-        { value: 'kaymilly Maia', label: 'kaymilly Maia' }
+    const months = [
+        { value: 1, label: 'Janeiro' },
+        { value: 2, label: 'Fevereiro' },
+        { value: 3, label: 'Março' }
+    ];
+
+    const years = [
+        { value: 2020, label: 2020 },
+        { value: 2019, label: 2019 },
+        { value: 2018, label: 2018 },
     ];
 
 
     return (
         <Container>
             <ContentHeader title="Saídas" lineColor="#F79390">
-                <SelectInput options={options} />
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
+
+            <Filters>
+                <button type="button" className="tag-filter tag-recorrentes"> Recorrentes </button> 
+
+                <button type="button" className="tag-filter tag-eventuais"> Eventuais </button>
+            </Filters>
+
 
             <Content>
                 {/* Cartão de Historico Financeiro */}
@@ -29,132 +43,7 @@ const List: React.FC = () => {
                     amount="R$ 79,00"
                 />
 
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
-                {/* Cartão de Historico Financeiro */}
-                <HistoryFinanceCard
-
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subTitle="01/12/2020"
-                    amount="R$ 79,00"
-                />
-
+                
             </Content>
         </Container>
     );
--- src/Pages/List/styles.ts
+++ src/Pages/List/styles.ts
@@ -1,4 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
 export const Content = styled.div``;
+
+/* Estilizando os Butões de filters */
+export const Filters = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom:30px;
+
+  /* criando uma class CSS */
+  .tag-filter {
+    font-size: 18px;
+    font-weight: 500;
+    background: none;
+    color: ${(props) => props.theme.colors.white};
+    margin: 0 10px;
+    transition: opacity 0.3s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+
+  .tag-recorrentes::after {
+    content: "";
+    display: block;
+    width: 55px;
+    margin: 0 auto;
+    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+  }
+
+  .tag-eventuais::after {
+    content: "";
+    display: block;
+    width: 55px;
+    margin: 0 auto;
+    border-bottom: 10px solid ${(props) => props.theme.colors.success};
+  }
+
+`;
